88c82006aa5a665c7fd1da9c8b2421db23071436
Cristian Iacob <cristian.iacob@cegeka.com>
2024-07-28 13:49:10 +0300

    create rest endpoint for creating a product

---
+++ src/main/java/com/products/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.products.controller;
+
+import com.products.entity.Product;
+import com.products.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    private ProductService productService;
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+
+        Product createdProduct = productService.create(product);
+        return ResponseEntity.ok(createdProduct);
+    }
+}
