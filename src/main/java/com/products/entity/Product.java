541869410911778b1c5f9789da151ec77df175da
Cristian Iacob <cristian.iacob@cegeka.com>
2024-07-28 13:47:51 +0300

    add product entity

---
+++ src/main/java/com/products/entity/Product.java
@@ -0,0 +1,33 @@
+package com.products.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+}
